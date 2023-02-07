fde3422402b8fe98624a86efca53eb4469089cda
nao1234 <ainanmikan@gmail.com>
2023-02-07 16:07:38 +0900

    ログイン必須のアクション作成＃DAY19

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,6 +27,5 @@ class Article < ApplicationRecord
     def validate_title_and_content_length
         char_count = self.title.length + self.content.length
         errors.add(:content, '100文字以上で!') unless char_count > 100
-        end
     end
 end
